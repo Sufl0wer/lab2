560b89268774d0f8eb2ad775759f734ff6cb678c
DevL0per <vazhnik.roma@gmail.com>
2020-05-09 00:58:22 +0300

    popUp alert was added

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function openForm() {
+  document.getElementById("myForm").style.display = "flex";
+  document.getElementById("popUp").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
